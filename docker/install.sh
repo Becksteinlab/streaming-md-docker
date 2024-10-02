04de3569eb4b07b46348de03d2de27f87c2206af
hmacdope <hugomacdermott@gmail.com>
2024-10-02 19:55:54 +1000

    progress

---
+++ docker/install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+export PATH="${PATH}:${CUDA_HOME}/bin"
+mamba install git cmake cudatoolkit gcc=11 gxx=11 -y
+
+# setup GMX 
+git clone https://github.com/HeydenLabASU/imd-test.git
+cd imd-test
+git checkout heekun_testruns
+pwd
+cd gromacs-2023.5 && mkdir build && cd build && cmake .. -DGMX_GPU=CUDA -DCUDA_TOOLKIT_ROOT_DIR=${CUDA_HOME} -DGMX_BUILD_OWN_FFTW=ON -DREGRESSIONTEST_DOWNLOAD=ON && make && make check && sudo make install
+source /usr/local/gromacs/bin/GMXRC
+gmx mdrun --help
+
+
+# Setup NAMD
