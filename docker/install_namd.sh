45f3e80e5916ae76fb57a3dd2586e6d9417cd4d7
hmacdope <hugomacdermott@gmail.com>
2024-10-17 23:18:48 +1100

    Get NAMD working and activate LAMMPS and NAMD

--- docker/install_namd.sh
+++ docker/install_namd.sh
@@ -2,7 +2,11 @@
 export PATH="${PATH}:${CUDA_HOME}/bin"
 cd /opt/
 export HOME_DIR=${PWD}
-export CPATH=${CONDA_PREFIX}/include
+# set include path to conda
+# export CPATH=${CONDA_PREFIX}/include
+# # set linker path to conda libs
+export LIBRARY_PATH=${CONDA_PREFIX}/lib
+export LD_LIBRARY_PATH=${CONDA_PREFIX}/lib
 export NPROC=$(nproc)
 
 # Setup NAMD
@@ -10,14 +14,9 @@ git clone https://github.com/amruthesht/namd-3.0.git
 cd namd-3.0
 tar xf charm-8.0.0.tar
 cd charm-8.0.0
-./build charm++ multicore-linux-x86_64 -j16 --with-production 
+./build charm++ multicore-linux-x86_64 -j ${NPROC} --with-production --libdir=${CONDA_PREFIX}/lib --incdir=${CONDA_PREFIX}/include
 cd ../
-wget http://www.ks.uiuc.edu/Research/namd/libraries/fftw-linux-x86_64.tar.gz
-tar xzf fftw-linux-x86_64.tar.gz
-mv linux-x86_64 fftw
-wget http://www.ks.uiuc.edu/Research/namd/libraries/tcl8.5.9-linux-x86_64-threaded.tar.gz
-tar xzf tcl8.5.9-linux-x86_64-threaded.tar.gz
-mv tcl8.5.9-linux-x86_64-threaded tcl-threaded
-./config Linux-x86_64-g++ --charm-arch multicore-linux-x86_64 --with-fftw3 --with-cuda --with-single-node-cuda
+./config Linux-x86_64-g++ --charm-arch multicore-linux-x86_64 --with-fftw3 --with-cuda  --tcl-prefix ${CONDA_PREFIX}  --cxx-opts -L${CONDA_PREFIX}/lib  --cc-opts -L${CONDA_PREFIX}/lib
 cd Linux-x86_64-g++
 make -j ${NPROC}
+ln -s /opt/namd-3.0/Linux-x86_64-g++/namd3  /bin/namd3
