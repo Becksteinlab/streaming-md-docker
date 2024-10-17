630b57cbe927999af0250f2d5c0bd42e89f2af4d
hmacdope <hugomacdermott@gmail.com>
2024-10-17 15:26:14 +1100

    modify confusing folder creation before simlink

--- docker/install_gromacs.sh
+++ docker/install_gromacs.sh
@@ -5,11 +5,11 @@ export HOME_DIR=${PWD}
 export CPATH=${CONDA_PREFIX}/include
 export NPROC=$(nproc)
 
-setup GMX 
+
 git clone https://github.com/HeydenLabASU/imd-test.git
 cd imd-test
 git checkout heekun_testruns
 pwd
-cd gromacs-2023.5 && mkdir build && cd build && cmake .. -DGMX_GPU=CUDA -DCUDA_TOOLKIT_ROOT_DIR=${CUDA_HOME} -DGMX_BUILD_OWN_FFTW=ON -DREGRESSIONTEST_DOWNLOAD=ON && make && make check && sudo make install
-source /usr/local/gromacs/bin/GMXRC
-gmx mdrun --help
+cd gromacs-2023.5 && mkdir build && cd build && cmake .. -DGMX_GPU=CUDA -DCUDA_TOOLKIT_ROOT_DIR=${CUDA_HOME} -DGMX_BUILD_OWN_FFTW=ON -DREGRESSIONTEST_DOWNLOAD=ON -DCMAKE_INSTALL_PREFIX=/usr/local/gromacs
+make -j ${NPROC}
+make install
