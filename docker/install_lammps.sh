9ad7c56f52b861ce513bb17553ce9bcf54f4906a
ljwoods2 <145226270+ljwoods2@users.noreply.github.com>
2024-10-18 17:54:16 -0700

    Switch branch built for lammps

--- docker/install_lammps.sh
+++ docker/install_lammps.sh
@@ -8,6 +8,7 @@ export NPROC=$(nproc)
 # Setup LAMMPS
 git clone https://github.com/ljwoods2/lammps.git
 cd lammps
+git checkout imd-v3
 mkdir build
 cd build
 cmake ../cmake/ -D PKG_MISC=yes -D PKG_H5MD=yes -D PKG_GPU=yes -DCMAKE_INSTALL_PREFIX=/opt/lammps_build
