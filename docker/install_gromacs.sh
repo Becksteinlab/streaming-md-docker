4ae687f9c8547457a94e14a657c5638028a5c326
ljwoods2 <145226270+ljwoods2@users.noreply.github.com>
2024-10-18 17:55:21 -0700

    Checkout imd-v3 branch after cd into repo dir

--- docker/install_gromacs.sh
+++ docker/install_gromacs.sh
@@ -7,8 +7,8 @@ export LIBRARY_PATH=${CONDA_PREFIX}/lib
 export LD_LIBRARY_PATH=${CONDA_PREFIX}/lib
 
 git clone https://github.com/hcho38/gromacs.git
-git checkout imd-v3
 cd gromacs
+git checkout imd-v3
 mkdir build
 cd build
 cmake .. -DGMX_GPU=CUDA -DCUDA_TOOLKIT_ROOT_DIR=${CUDA_HOME} -DGMX_BUILD_OWN_FFTW=ON -DREGRESSIONTEST_DOWNLOAD=OFF -DCMAKE_INSTALL_PREFIX=/opt/gromacs_build -DCMAKE_CXX_FLAGS="-L${CONDA_PREFIX}/lib -I${CONDA_PREFIX}/include"
